480e7ccc7d51edbb63a8bf54520349bea3c58919
Sivan Becker <sivanbecker@gmail.com>
2020-10-18 23:13:56 +0300

    tests wip and adding runtime file for heroku

--- tests/test_providers.py
+++ tests/test_providers.py
@@ -1,5 +1,22 @@
+import os
+import pytest
 from providers import __version__
+import tempfile
+from providers import providers
 
+
+@pytest.fixture
+def client():
+    db_fd, providers.app.config['DATABASE'] = tempfile.mkstemp()
+    providers.app.config['TESTING'] = True
+
+    with providers.app.test_client() as client:
+        with providers.app.app_context():
+            providers.init_db()
+        yield client
+
+    os.close(db_fd)
+    os.unlink(providers.app.config['DATABASE'])
 
 def test_version():
     assert __version__ == '0.1.0'
